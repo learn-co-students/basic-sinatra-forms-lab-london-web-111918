6c8c37ebaed6eb5466f266407767fb20dd5f499e
atkinsonholly <atkinsonholly@gmail.com>
2018-12-12 16:27:02 +0000

    Tests passing

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    @newteam = params["newteam"]
+    erb :team
+  end
+
+  # get '/team' do
+  #   erb :team
+  # end
 
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
 require './config/environment'
+require 'pry'
+
 
 run App
